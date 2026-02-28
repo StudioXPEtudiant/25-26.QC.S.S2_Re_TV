995b721318b8b6e32be1d737ac57eb818b9dd95f
Anyme_040 <Abdel-Tessala@hotmail.com>
2026-02-28 16:03:28 -0500

    changement player animation nouveau ne marche pas encore entrain de régler

---
+++ realplayer.gd
@@ -0,0 +1,40 @@
+extends Node3D
+
+@onready var anim = $AnimationPlayer
+@export var speed: float = 5.0
+@export var rotation_speed: float = 5.0
+func _process(delta):
+	var direction := Vector3.ZERO
+
+	# Input
+	if Input.is_action_pressed("L_Left"):
+		direction.x -= 1
+	if Input.is_action_pressed("L_Right"):
+		direction.x += 1
+	if Input.is_action_pressed("L_Up"):
+		direction.z += 1
+	if Input.is_action_pressed("L_Down"):
+		direction.z -= 1
+
+	# Normalisation
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+
+		# Rotation (modèle regardant vers -Z)
+		var target_angle := atan2(direction.x, -direction.z)
+		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
+	else:
+		# Pas de rotation si pas de mouvement
+		pass
+
+	# Mouvement global
+	var global_move := transform.basis * direction
+	translate(global_move * speed * delta)
+
+	# Animations
+	if direction != Vector3.ZERO:
+		if anim.current_animation != "Walk_Formal":
+			anim.play("Walk_Formal")
+	else:
+		if anim.current_animation != "Idle":
+			anim.play("Idle")
