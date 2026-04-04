d8e2a47c98f6130e0f57c6d5342f7cdcbadfbd3e
NohamLamarche <luc_jocelyne@hotmail.com>
2026-04-04 15:32:17 -0400

    Le commit du chase scene

---
+++ chase_launch_script.gd
@@ -0,0 +1,14 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+func body_entered(delta:float):
+	var new_node = Node2D.new() 
+	pass
