4710a6b305f1408ceb589e2fe7c52f2e35865a21
Anyme_040 <Abdel-Tessala@hotmail.com>
2026-04-04 15:31:10 -0400

    commit personnage finis

---
+++ camera_3d.gd
@@ -0,0 +1,13 @@
+extends Camera3D
+@onready var target: Node3D = $"../UAL1_Standard"   # Adjust path if needed
+@export var follow_speed: float = 5.0
+@export var offset: Vector3 = Vector3(0, 4, 6)
+
+func _process(delta: float) -> void:
+	if not target:
+		return
+
+	var desired_pos = target.global_transform.origin + offset
+	global_transform.origin = global_transform.origin.lerp(desired_pos, delta * follow_speed)
+
+	look_at(target.global_transform.origin, Vector3.UP)
